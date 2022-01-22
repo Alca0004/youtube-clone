6c9b3963c50310e49879c8168f48302e709cd5d7
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-01-22 17:30:23 -0500

    Building youtube-clone

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBiqBQfUZurf584FaEOHRcExAtim50zVa4',
+  authDomain: 'clone-e9666.firebaseapp.com',
+  projectId: 'clone-e9666',
+  storageBucket: 'clone-e9666.appspot.com',
+  messagingSenderId: '225247611217',
+  appId: '1:225247611217:web:46bad6d84af35053476b70',
+  measurementId: 'G-2YX9LMX5K8',
+};
