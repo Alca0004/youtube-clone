a36753f70e73d2481ba40dd6b08edd0b39ecd4aa
Aaron Alcala <84670853+Alca0004@users.noreply.github.com>
2022-01-24 11:40:15 -0500

    Header completed
    Starting with sidebar

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
 import AppsIcon from '@mui/icons-material/Apps';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import Avatar from '@material-ui/core/Avatar';
 
 function Header() {
   return (
-    <div className="Header">
-      <h1>I am a header</h1>
-      <MenuIcon />
-      <img
-        className="header__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1024px-YouTube_Logo_2017.svg.png"
-        alt=""
-      />
-      <input type="text" />
-      <SearchIcon />
-      <VideoCallIcon />
-      <AppsIcon />
-      <NotificationsIcon />
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1024px-YouTube_Logo_2017.svg.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__input">
+        <input placeholder="Search" type="text" />
+        <SearchIcon className="header__inputButton" />
+      </div>
+
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar
+          alt="Remmy Sharp"
+          src="https://avatars.githubusercontent.com/u/84670853?v=4"
+        />
+      </div>
     </div>
   );
 }
